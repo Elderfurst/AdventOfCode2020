c3c09ef546248609880320211245e677ac953019
Nicholas Anderson <njcanderson@gmail.com>
2020-12-01 11:04:40 -0800

    Add Day 1 Part 1 and 2 solutions

---
+++ Day1.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	inputArray := readInput()
+
+	partOne(inputArray)
+	partTwo(inputArray)
+}
+
+func readInput() []int {
+	// Open our input file
+	file, err := os.Open("Inputs/Day1.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	var lines []int
+
+	scanner := bufio.NewScanner(file)
+
+	// scanner.Scan() advances to the next line by default
+	for scanner.Scan() {
+		// Get each individual line
+		text := scanner.Text()
+
+		// Convert our string line into an int
+		convertedText, _ := strconv.Atoi(text)
+
+		// Add the newly converted int to our return value
+		lines = append(lines, convertedText)
+	}
+
+	return lines
+}
+
+func partOne(inputArray []int) {
+	// Double loop over our input and add the values together
+	for _, first := range inputArray {
+		for _, second := range inputArray {
+			// Once we find the two values that sum to 2020 we can return their product
+			if first + second == 2020 {
+				fmt.Println(first * second)
+
+				return
+			}
+		}
+	}
+}
+
+func partTwo(inputArray []int) {
+	// Triple loop over our input and add the values together
+	for _, first := range inputArray {
+		for _, second := range inputArray {
+			for _, third := range inputArray {
+				// Once we find the two values that sum to 2020 we can return their product
+				if first + second + third == 2020 {
+					fmt.Println(first * second * third)
+
+					return
+				}
+			}
+		}
+	}
+}
